a8d380ea29e3a5dbfa1054b419960abb400bc920
Danilo <danilomaioli@gmail.com>
2022-07-16 18:55:27 -0300

    Fix

--- Vehicle Siren Lights/source/Mod.cpp
+++ Vehicle Siren Lights/source/Mod.cpp
@@ -13,7 +13,7 @@
 
 bool Mod::m_DebugEnabled = false;
 bool Mod::m_IsSamp = false;
-std::string Mod::m_Version = "1.5";
+std::string Mod::m_Version = "1.5.0";
 OpenMenuKeybind Mod::m_OpenMenuKeybind = { 76, false, false, true };
 
 CVehicle* testVehicle = NULL;
